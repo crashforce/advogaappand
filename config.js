a20bfda0636af89fa262218054565f891180c520
Israel Wilson <israelwilson@gmail.com>
2017-08-17 01:06:32 -0300

    Envio versão 0.1 - Android

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'advogapp',
+		wp_ws_url : 'http://advogapp.israelwilson.com.br/wp-appkit-api/advogapp',
+		wp_url : 'http://advogapp.israelwilson.com.br',
+		theme : 'q-android',
+		version : '0.1',
+		app_title : 'AdvogApp',
+		app_platform : 'android',
+		gmt_offset : -3,
+		debug_mode : 'off',
+		auth_key : 'uYHU`??o`5wHgJ.MV93LvS@G[,06,G3FBuMeciY`hr9gDSPE6#FcwoPLtLe3yH12',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
